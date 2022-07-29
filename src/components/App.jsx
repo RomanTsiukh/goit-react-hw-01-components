b60b4971c70c71012f93d5fa4dc68e4ff87a4603
Roman <95884644+RomanTsiukh@users.noreply.github.com>
2022-07-30 00:28:50 +0300

    add friend

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import { Profile } from './Profile/Profile'
 import { StatisticsList } from './Statistics/StatisticsList';
+import { FriendList } from './Friends/FriendList';
 import user from './Profile/user.json';
 import stats from './Statistics/data.json';
+import friends from './Friends/friends.json';
 
 
 export function App() {
@@ -10,6 +12,8 @@ export function App() {
         <Profile link={user.avatar} usname={user.username} ustag={user.tag} locat={user.location} folowers={user.stats.followers} views={user.stats.views} likes={user.stats.likes}/>
         
         <StatisticsList title="Upload stats" stats={stats}/>
+
+        <FriendList friends={friends}/>
       </div>
     )};
 
